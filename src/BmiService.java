ea3e409933592506580a0594515420785218d9ba
Anna Shcherbakova <ShcherbakovaAnna90@yandex.ru>
2023-11-14 14:23:52 +0600

    Second commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
-    public int calculate (double weightKg, double highMt) {
+    public int calculate(double weightKg, double highMt) {
         double index = weightKg / (highMt * highMt);
         return (int) index;
     }
